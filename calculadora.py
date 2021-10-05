e39957dc9e5e850ee7401899922d9742ac2d3f92
oscarmao <oscar.gonzalez.mt@correo.usa.edu.co>
2021-10-04 23:51:22 -0500

    proyecto base con funcion sumar

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+
+
+print("calculadora ejemplo")
+print()
+print("1). SUMAR")
+
+def sumar(n1,n2):
+    return n1+n2
+
+op=int(input("digite una opcion: "))
+n1=int(input("digite el numero: "))
+n2=int(input("Digite el numero 2: "))
+res=None
+if op==1:
+    res=sumar(n1,n2)
+    
+print("el resultado es: ", res)
